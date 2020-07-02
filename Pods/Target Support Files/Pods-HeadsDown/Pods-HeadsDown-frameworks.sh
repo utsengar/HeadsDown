0e376bbf68e4a9d3d3caabe40aeaeefc83aafe76
Utkarsh Sengar <utkarsh2012@gmail.com>
2020-07-01 20:46:01 -0700

    Add auto layouts for update button

--- Pods/Target Support Files/Pods-HeadsDown/Pods-HeadsDown-frameworks.sh
+++ Pods/Target Support Files/Pods-HeadsDown/Pods-HeadsDown-frameworks.sh
@@ -199,12 +199,10 @@ fi
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Sentry/Sentry.framework"
   install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
-  install_dsym "${PODS_ROOT}/Sparkle/Sparkle.framework.dSYM"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Sentry/Sentry.framework"
   install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
-  install_dsym "${PODS_ROOT}/Sparkle/Sparkle.framework.dSYM"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
